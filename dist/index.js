5f14ab26c1f4787becb7a721eef671c66038fd86
DESKTOP-UJFSGPE\User <setok0517@gmail.com>
2025-09-30 19:01:23 +0300

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let userName = "Ніка";
+let userAge = 21;
+let isStudent = true;
+console.log(`Ім'я: ${userName}`);
+console.log(`Вік: ${userAge}`);
+console.log(`Студент: ${isStudent}`);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let userName: string = "Ніка";
+let userAge: number = 21;
+let isStudent: boolean = true;
+
+console.log(`Ім'я: ${userName}`);
+console.log(`Вік: ${userAge}`);
+console.log(`Студент: ${isStudent}`);
