38a7256c539a4510571042004a2437f52e0f68d0
MeefiTyan <117981541+MeefiTyan@users.noreply.github.com>
2025-09-30 19:16:10 +0300

    Delete src directory

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-let userName: string = "Ніка";
-let userAge: number = 21;
-let isStudent: boolean = true;
-
-console.log(`Ім'я: ${userName}`);
-console.log(`Вік: ${userAge}`);
-console.log(`Студент: ${isStudent}`);
