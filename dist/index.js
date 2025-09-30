23fb3cac8ba911c65d118e71952ac7bd107ff444
MeefiTyan <117981541+MeefiTyan@users.noreply.github.com>
2025-09-30 19:15:45 +0300

    Delete dist directory

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let userName = "Ніка";
-let userAge = 21;
-let isStudent = true;
-console.log(`Ім'я: ${userName}`);
-console.log(`Вік: ${userAge}`);
-console.log(`Студент: ${isStudent}`);
